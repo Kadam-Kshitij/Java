e7d6fcd169969e30a732b5d55749071a264bf45d
Kshitij Kadam <54320682+Kadam-Kshitij@users.noreply.github.com>
2022-09-27 11:05:09 +0530

    Create Constructor.java

---
+++ Constructor/Constructor.java
@@ -0,0 +1,28 @@
+class MyClass {
+    // Making constructor private allows its object creation only from within the class
+    private MyClass() {
+        System.out.println( "Constructor 1" );
+    }
+    
+    MyClass( int i ) {
+        System.out.println( "Constructor 2" );
+    }
+    
+    void foo() {
+        // Can create since foo is within the class
+        MyClass o = new MyClass();
+    }
+}
+
+class Main {
+    public static void main( String[] str ) {
+        // MyClass obj1 = new MyClass(); // CTE - because no-arg constructor is private
+        MyClass obj2 = new MyClass( 23 );
+        
+        obj2.foo();
+    }
+}
+
+// -- Output --
+// Constructor 2
+// Constructor 1
