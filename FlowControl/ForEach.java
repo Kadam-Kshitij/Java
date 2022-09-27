bb826e5da25f486ed984b9bca4a038c6e8a95319
Kshitij Kadam <54320682+Kadam-Kshitij@users.noreply.github.com>
2022-09-27 10:01:14 +0530

    Create ForEach.java

---
+++ FlowControl/ForEach.java
@@ -0,0 +1,19 @@
+public class Main
+{
+	public static void main( String[] str ) {
+	    int[] arr = { 34, 45, 14, 77, 2, 434 };
+	    
+	    for ( int i : arr )
+	    {
+	        System.out.println( i );
+	    }
+	}
+}
+
+// -- Output --
+// 34
+// 45
+// 14
+// 77
+// 2
+// 434
